6b30c54e79910e23ff3808eece909a2849587ef4
Daniela <daniela.arroyo27@gmail.com>
2020-03-14 14:30:49 -0500

    query db start

--- Mar11/app.js
+++ Mar11/app.js
@@ -8,8 +8,18 @@ let initOptions = {
 let config = {
     host: 'localhost',
     port: 5432,
-    database: 'testdb',
+    database: 'restaurants',
     user: 'postgres'
 }
 
 //load and init pg-promise
+
+let pgp = require('pg-promise')(initOptions);
+let db = pgp(config);
+
+// console.log(db)
+
+db.query("SELECT * FROM restaurants WHERE category='LATIN'")
+    .then((results) => {
+        console.log(results)
+    })
