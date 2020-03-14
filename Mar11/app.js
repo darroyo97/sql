5ee2b37c683c2affb429bcfc87795528b87be3c5
Daniela <daniela.arroyo27@gmail.com>
2020-03-14 15:52:37 -0500

    query return

--- Mar11/app.js
+++ Mar11/app.js
@@ -31,10 +31,19 @@ let db = pgp(config);
 //     .then(() => {
 //         console.log('record was insterted')
 //     })
-db.query("SELECT * FROM pgpromise")
-    .then((results) => {
-        results.forEach((person) => {
-            console.log(`Good morning ${person.name}`)
-        })
-        // console.log(results)
+// db.query("SELECT * FROM pgpromise")
+//     .then((results) => {
+//         results.forEach((person) => {
+//             console.log(`Good morning ${person.name}`)
+//         })
+//         // console.log(results)
+//     })
+db.one("INSERT INTO pgpromise VALUES (DEFAULT,'Jovers', 'New York' ) RETURNING id")
+    .then((result) => {
+        console.log(result)
+        db.query(`SELECT * FROM pgpromise WHERE ID =${result.id}`)
+            .then((results) => {
+                console.log(`return from query`)
+            })
+        console.log('record was insterted')
     })
